03b42f5b54e412154dfd7e7d756490848012c41f
abdooimakol-svg <abdooimakol@gmail.com>
2026-06-22 16:24:40 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  nitro: true,
+
   tanstackStart: {
     server: { entry: "server" },
   },
 
   vite: {
     preview: {
-      allowedHosts: true
-    }
-  }
+      allowedHosts: true,
+    },
+  },
 });
