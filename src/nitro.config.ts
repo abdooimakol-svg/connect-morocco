8ec170ead7fb28d3fb4ddb0f46e442508182d140
abdooimakol-svg <abdooimakol@gmail.com>
2026-06-23 03:02:31 +0100

    Update nitro.config.ts

--- src/nitro.config.ts
+++ src/nitro.config.ts
@@ -2,5 +2,9 @@ import { defineNitroConfig } from "nitropack/config";
 
 export default defineNitroConfig({
   preset: "node-server",
+  entry: "./server.ts",
   compatibilityDate: "2024-01-01",
+  runtimeConfig: {
+    port: process.env.PORT,
+  },
 });
